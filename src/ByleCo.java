6d6e6b4109264f13c1ae8b58255ec2221ce3962e
spidomisiek88 <michal.pajak88@gmail.com>
2019-03-08 15:27:25 +0100

    Made spidomisiek88 branch

--- src/ByleCo.java
+++ src/ByleCo.java
@@ -9,5 +9,7 @@ public class ByleCo {
         System.out.println("kdsnskjnksjfnskjfnsjkfn");
 
         System.out.println("whatever");
+
+        System.out.println("Nowa zmiana Michala");
     }
 }
