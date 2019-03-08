3dd925f275afadb5307e4a765a3c4b5b3180fafc
kedudj <kedudj@gmail.com>
2019-03-08 15:40:50 +0100

    galaz nowa

--- src/DiffClass.java
+++ src/DiffClass.java
@@ -2,5 +2,7 @@ public class DiffClass {
 
     public static void main(String[] args) {
         System.out.println("inna klasa +5 +5");
+
+        System.out.println("sqvbhlfvbljvbljfv");
     }
 }
